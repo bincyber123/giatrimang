cbad1c23eeee4c84050a2b9410fe2402de266806
bincyber123 <conduongchuyenghiep@gmail.com>
2022-09-16 13:51:37 +0700

    first commit

---
+++ giatrimang.js
@@ -0,0 +1,8 @@
+let value = prompt("Nhap gia tri :");
+let x = [-3, 5, 1, 3, 2, 10];
+for (let i=0;i < x.length;i++){
+    if (value == x[i]){
+        alert("value" + x[i] + "gia tri" +i);
+
+    }
+}
